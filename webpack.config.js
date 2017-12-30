20055698f4260e0bc95e79bb1f3a8a48f93620bd
uhyo <uhyo@uhy.ooo>
2017-12-31 04:10:18 +0900

    production

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,21 @@
 'use strict';
 
+const {
+  LicenseWebpackPlugin,
+} = require('license-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+
+const plugins = [];
+if (process.env.NODE_ENV === 'production'){
+  plugins.push(new UglifyJsPlugin());
+  plugins.push(new LicenseWebpackPlugin({
+    pattern: /.*/,
+    unacceptablePattern: /GPL/,
+    abortOnUnacceptableLicense: true,
+  }));
+}
+
 module.exports = {
   devtool: process.env.NODE_ENV === 'production' ? false : 'source-map',
   entry: null,
@@ -13,6 +29,7 @@ module.exports = {
       },
     ],
   },
+  plugins,
   watchOptions: {
     aggregateTimeout: 200,
     ignored: /node_modules/,
